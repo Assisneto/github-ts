05472a19921059fc3c36abbcafdc769dfa8bd05d
Assisneto Damasceno <netodamasceno12@gmail.com>
2020-05-25 12:40:58 -0300

    Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/32393870?s=460&u=b74e0eb482dfe985720af46759fd99594576a5da&v=4"
+            alt="Assisneto Damasceno"
+          />
+          <div>
+            <strong>Assisneto/my-ts-boilerplate</strong>
+            <p>
+              boilerplate for backend applications with ts (in improvements)
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/32393870?s=460&u=b74e0eb482dfe985720af46759fd99594576a5da&v=4"
+            alt="Assisneto Damasceno"
+          />
+          <div>
+            <strong>Assisneto/my-ts-boilerplate</strong>
+            <p>
+              boilerplate for backend applications with ts (in improvements)
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/32393870?s=460&u=b74e0eb482dfe985720af46759fd99594576a5da&v=4"
+            alt="Assisneto Damasceno"
+          />
+          <div>
+            <strong>Assisneto/my-ts-boilerplate</strong>
+            <p>
+              boilerplate for backend applications with ts (in improvements)
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
